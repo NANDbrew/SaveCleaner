6a9bc377ad3d78298eb9e6253c49b6836e6e5ac5
NANDbrew <zortman@gmail.com>
2025-02-18 21:32:44 -0600

    improve cleaning of logged missions and trader boat data. add in-game notification after a save is cleaned

--- SaveCleaner.cs
+++ SaveCleaner.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -8,11 +9,20 @@ namespace SaveCleaner
 {
     internal static class SaveCleaner
     {
+        public static TraderBoatData[] referenceTraderData;
+        public static int changes = 0;
         public static SaveContainer CleanSave(SaveContainer saveContainer)
         {
+            referenceTraderData = new TraderBoatData[saveContainer.traderBoatData.Length];
+            for (int i = 0; i < referenceTraderData.Length; i++)
+            {
+                referenceTraderData[i] = saveContainer.traderBoatData[i];
+            }
+            Debug.Log("trader boat data length = " + saveContainer.traderBoatData.Length);
+            changes = 0;
             saveContainer.savedPrefabs.RemoveAll(prefab => !IsValidPrefab(prefab.prefabIndex) || (prefab.itemParentObject > -1 && !IsValidSaveableObject(prefab.itemParentObject)));
             saveContainer.savedObjects.RemoveAll(obj => !IsValidSaveableObject(obj.sceneIndex));
-            saveContainer.loggedMissions.Do(mission => { if (!Enum.IsDefined(typeof(PortRegion), mission.repRegion) || !IsValidPrefab(mission.goodIndex)) mission.goodIndex = 0; });
+            saveContainer.loggedMissions = CleanLoggedMissions(saveContainer.loggedMissions);
             saveContainer.traderBoatData = CleanTraderBoatData(saveContainer.traderBoatData);
             saveContainer.savedMissions = CleanMissionData(saveContainer.savedMissions);
 
@@ -23,6 +33,7 @@ namespace SaveCleaner
                     objectData.customization = CleanCustomizationData(objectData.customization, objectData.sceneIndex);
                 }
             }
+            NotificationUi.instance.StartCoroutine(ShowNotificationAfterDelay($"Save Cleaned\n\nfixed {changes} issues"));
             return saveContainer;
         }
 
@@ -33,16 +44,19 @@ namespace SaveCleaner
             if (data.masts.Length > refs.masts.Length)
             {
                 Array.Resize(ref data.masts, refs.masts.Length);
+                //changes++;
             }
             if (data.partActiveOptions.Count > parts.availableParts.Count)
             {
                 data.partActiveOptions.RemoveRange(parts.availableParts.Count, data.partActiveOptions.Count - parts.availableParts.Count);
+                //changes++;
             }
             for (int i = 0; i < data.partActiveOptions.Count; i++)
             {
                 if (data.partActiveOptions[i] >= parts.availableParts[i].partOptions.Count)
                 {
                     data.partActiveOptions[i] = parts.availableParts[i].activeOption;
+                    //changes++;
                 }
             }
             for (int i = 0; i < data.sails.Count;)
@@ -51,10 +65,12 @@ namespace SaveCleaner
                 if (sail.sailColor >= PrefabsDirectory.instance.sailColors.Length)
                 {
                     sail.sailColor = 0;
+                    //changes++;
                 }
                 if (sail.mastIndex >= refs.masts.Length || refs.masts[sail.mastIndex] == null || sail.prefabIndex >= PrefabsDirectory.instance.sails.Length || PrefabsDirectory.instance.sails[sail.prefabIndex] == null)
                 {
                     data.sails.Remove(sail);
+                    changes++;
                     continue;
                 }
                 i++;
@@ -68,27 +84,67 @@ namespace SaveCleaner
             for (int i = 0; i < incomingData.Length; i++)
             {
                 TraderBoatData traderBoatData = incomingData[i];
-                /*if (traderBoatData.carriedPriceReports.Length > Port.ports.Length)
-                {
-                    Array.Resize(ref traderBoatData.carriedPriceReports, Port.ports.Length);
-                }*/
-                //traderBoatData.carriedPriceReports = new PriceReport[0];
                 bool brokenIndex = false;
-                foreach (int index in traderBoatData.carriedGoods)
+
+                for (int j = 0; j < traderBoatData.carriedGoods.Length; j++)
                 {
-                    if (!IsValidPrefab(index))
+                    int index = traderBoatData.carriedGoods[j];
+                    if (index > 0 && !IsValidPrefab(index))
                     {
                         brokenIndex = true;
-                        break;
+                        traderBoatData.carriedGoods[j] = 0;
+                        //changes++;
+                        //break;
                     }
                 }
-                if (!brokenIndex && IsValidPort(traderBoatData.currentDestination) && IsValidPort(traderBoatData.lastIslandMarket) && IsValidPort(traderBoatData.currentIslandMarket))
+                if (brokenIndex) changes++;
+                if (!IsValidPort(traderBoatData.currentIslandMarket))
+                {
+                    traderBoatData.currentIslandMarket = -1;
+                    //changes++;
+                }
+                if (!IsValidPort(traderBoatData.currentDestination))
+                {
+                    traderBoatData.currentDestination = -1;
+                    //changes++;
+                }
+
+                if (/*!brokenIndex && */IsValidPort(traderBoatData.lastIslandMarket))
                 {
                     outgoingData.Add(traderBoatData);
+                }
+                else
+                {
+                    changes++;
+                    Debug.LogError("removed trader boat " + i);
                 }
             }
             return outgoingData.ToArray();
         }
+
+        public static LoggedMission[] CleanLoggedMissions(LoggedMission[] savedMissions)
+        {
+            //bool changed = false;
+            for (int i = 0; i < savedMissions.Length; i++)
+            {
+                LoggedMission mission = savedMissions[i];
+                if (!IsValidPrefab(mission.goodIndex))
+                {
+                    mission.goodIndex = 0;
+                    //changes++;
+                    //changed = true;
+                }
+                if (!Enum.IsDefined(typeof(PortRegion), mission.repRegion))
+                {
+                    mission.repRegion = (int)PortRegion.none;
+                    changes++;
+                    //changed = true;
+                }
+            }
+            //changes += changed ? 1 : 0;
+            return savedMissions;
+        }
+
 
         public static SaveMissionData[] CleanMissionData(SaveMissionData[] savedMissions)
         {
@@ -101,6 +157,7 @@ namespace SaveCleaner
                     continue;
                 }
                 savedMissions[i] = null;
+                changes++;
             }
             return savedMissions;
         }
@@ -116,6 +173,14 @@ namespace SaveCleaner
         public static bool IsValidPort(int portIndex)
         {
             return portIndex >= 0 && portIndex < Port.ports.Length && Port.ports[portIndex] != null;
+        }
+
+        public static IEnumerator ShowNotificationAfterDelay(string text)
+        {
+            yield return new WaitUntil(() => GameState.playing && !GameState.justStarted);
+            //yield return new WaitForSeconds(5);
+            NotificationUi.instance.ShowNotification(text);
+            //Debug.Log("displayed notification?");
         }
     }
 }
